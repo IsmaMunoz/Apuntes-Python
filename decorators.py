23a29d2e32e9d00d50c453910a91a365b19006eb
Isma Muñoz <munoz.isma@gmail.com>
2022-07-12 15:30:09 -0500

    Decoradores de funciones en python

--- decorators.py
+++ decorators.py
@@ -5,17 +5,22 @@
 from datetime import datetime
 
 def execution_time(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         initial_time = datetime.now()
-        func()
+        func(*args, **kwargs)
         final_time = datetime.now()
-        time_elapsed = final.time - initial.time
+        time_elapsed = final_time - initial_time
         print("Pasaron " + str(time_elapsed.total_seconds()) + " segundos")
-        return wrapper
+    return wrapper
 
 @execution_time
 def random_func():
-    for _ in range(1, 1000000):
+    for _ in range(1, 100000000):
         pass
 
+@execution_time
+def suma (a: int, b: int) -> int:
+    return a + b
+
+suma (5 , 5)
 random_func()
